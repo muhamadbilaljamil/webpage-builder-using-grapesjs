0ed81c98cf8c937868e7076a2876435854ce4345
bilal jamil <muhamadbilaljamil@gmail.com>
2024-01-17 19:02:21 +0500

    configuration

--- src/components/PageBuilder/PageBuilder.tsx
+++ src/components/PageBuilder/PageBuilder.tsx
@@ -1,27 +1,22 @@
 //@ts-ignore
 "use client";
 import React, { useState, useEffect } from "react";
-import grapesjs from "grapesjs";
-import grapesPresetWebpage from "grapesjs-preset-webpage";
+// import grapesjs from "grapesjs";
 import "grapesjs/dist/css/grapes.min.css";
 
 const PageBuilder = () => {
-  const [editor, setEditor] = useState(null);
+  // const [editor, setEditor] = useState(null);
 
-  useEffect(() => {
-    const editor = grapesjs.init({
-      container: "#editor",
-      plugins: [grapesPresetWebpage],
-      pluginsOpts: {
-        grapesPresetWebpage: {},
-      },
-    });
-    setEditor(editor);
-  }, []);
+  // useEffect(() => {
+  //   const editor = grapesjs.init({
+  //     container: "#editor",
+  //   });
+  //   setEditor(editor);
+  // }, []);
 
   return (
     <div>
-      <div id="editor"></div>
+      {/* <div id="editor"></div> */}
     </div>
   );
 };
